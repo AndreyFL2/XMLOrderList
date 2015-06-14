f4eeef2873f636a132492eda67978845b1a2219f
AndreyFL2 <AndreyFL@live.com>
2015-06-14 10:01:58 +0300

    First Issue

---
+++ XMLOrderList/Order.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLOrderList
+{
+    class Order
+    {
+        class Product
+        {
+            string name;
+            public uint amount;
+            public uint price;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public uint Amount
+            {
+                get
+                { return amount; }
+                set
+                { amount = value; }
+            }
+            public uint Price
+            {
+                get
+                { return price; }
+                set
+                { price = value; }
+            }
+
+        }
+
+        List<Product> Products;
+        string Phone { get; set; }
+
+        public Order()
+        {
+            Products = new List<Product>();
+        }
+
+        public XmlNode Make(XmlDocument doc)
+        {
+            Console.Write("Введите номер телефона заказчика: ");
+            Phone = Console.ReadLine();
+            if (Phone.Length <= 0)
+            {
+                return null;
+            }
+
+            ConsoleKey key;
+            do
+            {
+                key = ConsoleKey.N;
+                Product prod = new Product();
+
+                Console.Write("Введите наименование продукта: ");
+                prod.Name = Console.ReadLine();
+                if (prod.Name.Length <= 0)
+                {
+                    return null;
+                }
+                try
+                {
+                    Console.Write("Введите количество продукта: ");
+                    prod.Amount = Convert.ToUInt32(Console.ReadLine());
+                    Console.Write("Введите стоимость продукта: ");
+                    prod.Price = Convert.ToUInt32(Console.ReadLine());
+
+                    Products.Add(prod);
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+                Console.WriteLine("Добавить еще одину позицию заказа ? [Y/N]: ");
+                key = Console.ReadKey(true).Key;
+            } while (key.ToString() == "y" || key.ToString() == "Y");
+
+            if (Products.Count > 0)
+            {
+                XmlNode order = doc.CreateElement("order");
+                foreach (Product pr in Products)
+                {
+                    XmlNode product = doc.CreateElement("product");
+
+                    XmlNode productName = doc.CreateElement("name");
+                    XmlNode productNameValue = doc.CreateTextNode(pr.Name);
+                    productName.AppendChild(productNameValue);
+                    product.AppendChild(productName);
+
+                    XmlNode productAmount = doc.CreateElement("amount");
+                    XmlNode productAmountValue = doc.CreateTextNode(pr.Amount.ToString());
+                    productAmount.AppendChild(productAmountValue);
+                    product.AppendChild(productAmount);
+
+                    XmlNode productPrice = doc.CreateElement("price");
+                    XmlNode productPriceValue = doc.CreateTextNode(pr.Price.ToString());
+                    productPrice.AppendChild(productPriceValue);
+                    product.AppendChild(productPrice);
+
+                    order.AppendChild(product);
+
+                }
+                XmlNode clientsPhone = doc.CreateElement("clientsPhone");
+                XmlNode clientsPhoneValue = doc.CreateTextNode(this.Phone);
+                clientsPhone.AppendChild(clientsPhoneValue);
+                order.AppendChild(clientsPhone);
+
+                return order;
+            }
+            else
+            { return null; }
+        }
+    }
+}
---
+++ XMLOrderList/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLOrderList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument(); 
+            try
+            {
+                // Пробую открыть xml документ, и в случае успеха передаю полученный экземпляр XmlDocument в метод для добавления заказа в существующий файл
+                doc.Load("Orders.xml");
+                WriteOrderToXmlFile(doc);
+            }
+            catch (Exception ex)
+            {
+                // В случае ошибок, создаю новый путем вызова метода с передачей null вместо экземпляра XmlDocument
+                WriteOrderToXmlFile(null);
+                Console.WriteLine(ex.Message);
+            }
+
+            ShowNode(doc.DocumentElement);
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey(true);
+        }
+
+
+        /// <summary>
+        /// Метод используемый для добавления нового заказа в существующий xml документ, или создание нового xml документа.
+        /// </summary>
+        /// <param name="doc">Передается ссылка на экземпляр XmlDocument, если нужно добавить заказ в существующий файл, либо null в случае если формируется новый xml файл.</param>
+        static void WriteOrderToXmlFile(XmlDocument doc)
+        {
+            Console.Write("Хотите добавить новый заказ ? [Y/N]: ");
+            ConsoleKey key = Console.ReadKey(true).Key;
+            if (key.ToString() != "Y" && key.ToString() != "y")
+            {
+                ShowNode(doc.DocumentElement);
+                return;
+            }
+            Console.WriteLine();
+
+            XmlNode rootElem;
+            if (doc == null)
+            {
+                doc = new XmlDocument();
+                rootElem = doc.CreateElement("orders");
+                doc.AppendChild(rootElem);
+            }
+            else
+            {
+                rootElem = doc.DocumentElement;
+            }
+
+            // Создаю экземпляр нового заказа
+            Order orderObj = new Order();
+            // Получаю новый заказ в виде XmlNode
+            XmlNode order = orderObj.Make(doc);
+
+            // Если заказ сформирован корректно (не null) добавляю его в файл xml и записываю документ на диск.
+            if (order != null)
+            {
+                // Вычисляю номер для нового заказа
+                XmlNodeList orders = rootElem.ChildNodes;
+                uint orderID = 0; // ID нового заказа
+                // ID нового заказа = (максимальный номер среди уже созданных заказов) +1
+                foreach (XmlNode node in orders)
+                {
+                    XmlAttributeCollection attributes = node.Attributes;
+                    foreach (XmlAttribute attr in attributes)
+                    {
+                        if ((attr.Name == "id") && (orderID <= Convert.ToUInt32(attr.Value))) { orderID = Convert.ToUInt32(attr.Value) + 1; }
+                    }
+                }
+
+                // Добавляю к заказу атрибут ID
+                XmlAttribute orderIDAttr = doc.CreateAttribute("id");
+                orderIDAttr.Value = orderID.ToString();
+                order.Attributes.Append(orderIDAttr);
+
+                // Добавляю новый заказ к xml документу
+                rootElem.AppendChild(order);
+
+                doc.Save("Orders.xml");
+                doc.Load("Orders.xml");
+            }
+            else {
+                Console.WriteLine("Error, new order hasn't been added !!! Press ane key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Вывод списка заказов на консоль
+        /// </summary>
+        /// <param name="node"></param>
+        static void ShowNode(XmlNode orders)
+        {
+            foreach (XmlNode order in orders)
+            {
+                Console.WriteLine();
+                if (order.Attributes != null)
+                {
+                    foreach (XmlAttribute attr in order.Attributes)
+                    {
+                        Console.WriteLine("Order ID = {0}", attr.Value);
+                        Console.WriteLine("================");
+                    }
+                }
+                if (order.HasChildNodes)
+                {
+                    XmlNodeList products = order.ChildNodes;
+                    foreach (XmlNode product in products)
+                    {
+                        //Console.WriteLine(product.Name);
+                        if (product.HasChildNodes)
+                        {
+                            XmlNodeList productFields = product.ChildNodes;
+                            foreach (XmlNode productField in productFields)
+                            {
+                                if (productField.NodeType == XmlNodeType.Element)
+                                {
+                                    Console.Write(" {0} = {1},", productField.Name, productField.InnerText);
+                                }
+                                else {
+                                    Console.Write(" {0} = {1}", product.Name, product.InnerText);
+                                }
+                            }
+                            Console.WriteLine();
+                        }
+                        else {
+                         
+                        }
+                        //Console.Write(" {0} = {1}", product.Name, product.Value);
+                    }
+                }
+            }
+        }
+    }
+}
